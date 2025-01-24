09fd3ca67ad715b8f19e0d8c0a0ee4cd48136c6b
HenriqueSilva00 <Henrique_borges_silva@hotmail.com>
2025-01-24 20:09:51 +0000

    Inserção da galeria

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { FaHome, FaBook, FaBars, FaPhone } from 'react-icons/fa'; // Exemplo de ícones do Font Awesome
+import React, { useState, useEffect, useCallback } from "react";
+import { FaHome, FaBook, FaBars, FaPhone } from "react-icons/fa";
 
 const Header = () => {
   const [isVisible, setIsVisible] = useState(true);
   const [lastScrollY, setLastScrollY] = useState(0);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const handleScroll = () => {
+  // Memoriza a função handleScroll para evitar recriação em cada renderização
+  const handleScroll = useCallback(() => {
     const currentScrollY = window.scrollY;
     if (currentScrollY > lastScrollY) {
       setIsVisible(false); // Esconde o header
@@ -14,44 +15,47 @@ const Header = () => {
       setIsVisible(true); // Mostra o header
     }
     setLastScrollY(currentScrollY);
-  };
+  }, [lastScrollY]); // Dependência de lastScrollY
 
   const scrollToSection = (sectionId) => {
     const element = document.getElementById(sectionId);
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      element.scrollIntoView({ behavior: "smooth", block: "start" });
     }
   };
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  }, [lastScrollY]);
+  }, [handleScroll]); // Adicione handleScroll como dependência
 
   return (
     <header className={`header ${isVisible ? "visible" : "hidden"}`}>
       <div className="title_container">
         <h1 className="title">Leopoldina Website</h1>
       </div>
-      <nav>
-        
-        <div className={`icon_container ${isMenuOpen ? "open" : ""}`}>
-            <button className="icon-button" onClick={() => scrollToSection('home')}>
-              <FaHome size={28} /> {/* Ícone de Home */}
-            </button>
-            <button className="icon-button" onClick={() => scrollToSection('history')}>
-              <FaBook size={28} /> {/* Ícone de Search */}
-            </button>
-            <button className="icon-button" onClick={() => scrollToSection('contacts')}>
-              <FaPhone size={28} /> {/* Ícone de User */}
-            </button>
-        </div>
-        <div className="menu-toggle" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+      <div className={`icon_container ${isMenuOpen ? "open" : ""}`}>
+        <button className="icon-button" onClick={() => scrollToSection("home")}>
+          <FaHome size={28} /> {/* Ícone de Home */}
+        </button>
+        <button
+          className="icon-button"
+          onClick={() => scrollToSection("history")}
+        >
+          <FaBook size={28} /> {/* Ícone de Search */}
+        </button>
+        <button
+          className="icon-button"
+          onClick={() => scrollToSection("contacts")}
+        >
+          <FaPhone size={28} /> {/* Ícone de User */}
+        </button>
+      </div>
+      <div className="menu-toggle" onClick={() => setIsMenuOpen(!isMenuOpen)}>
         <button>
-              <FaBars size={28} /> {/* Ícone de User */}
-            </button>
-        </div>
-      </nav>
+          <FaBars size={28} /> {/* Ícone de Menu */}
+        </button>
+      </div>
     </header>
   );
 };
--- src/components/maincontent.js
+++ src/components/maincontent.js
@@ -1,16 +1,60 @@
 import React from "react";
+import { Carousel } from "react-carousel-minimal"; // Exemplo de carrossel
+import image1 from "../assets/montanha.jpeg";
+import image2 from "../assets/rio.jpg";
+import image3 from "../assets/lago.jpg";
 
 const MainContent = () => {
+  const data = [
+    {
+      image: image1,
+      caption: "1º Foto",
+    },
+    {
+      image: image2,
+      caption: "2º Foto",
+    },
+    {
+      image: image3,
+      caption: "3º Foto",
+    },
+  ];
+
   return (
     <main>
       <section id="home" className="section section1">
         <h2>Secção 1</h2>
         <p>Conteúdo da primeira secção.</p>
       </section>
+
       <section id="history" className="section section2">
-        <h2>Secção 2</h2>
-        <p>Conteúdo da segunda secção.</p>
+        <div className="carousel-container">
+          <h1 className="carousel-title">Galeria de Fotos</h1>
+          <div className="carousel-wrapper">
+            <Carousel
+              data={data}
+              time={2000}
+              width="100%"
+              height="100%"
+              captionStyle={{
+                fontSize: "2em",
+                fontWeight: "bold",
+                color: "#fff",
+              }}
+              radius="10px"
+              captionPosition="bottom"
+              automatic={false}
+              dots={true}
+              pauseIconColor="white"
+              pauseIconSize="80px"
+              slideImageFit="cover"
+              thumbnails={true}
+              thumbnailWidth="100px"
+            />
+          </div>
+        </div>
       </section>
+
       <section id="contacts" className="section section3">
         <h2>Secção 3</h2>
         <p>Conteúdo da terceira secção.</p>

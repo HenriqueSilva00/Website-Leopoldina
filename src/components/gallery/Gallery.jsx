89154e64348898237d8e9d4ae501cd7f03c42ab9
HenriqueSilva00 <Henrique_borges_silva@hotmail.com>
2025-11-04 16:04:04 +0000

    issue3

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -2,21 +2,6 @@ import React, { useEffect, useState } from "react";
 
 import "./Gallery.css"; // CSS específico da galeria
 
-const Gallery = () => {
-  const [htmlContent, setHtmlContent] = useState("");
-
-  useEffect(() => {
-    fetch("/view/gallery/gallery.html")
-      .then((res) => res.text())
-      .then((data) => setHtmlContent(data))
-      .catch((err) => console.error("Erro ao carregar galeria:", err));
-  }, []);
-
-  return (
-    <section id="gallery" className="section gallery">
-      <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
-    </section>
-  );
-};
+const Gallery = () => {};
 
 export default Gallery;
